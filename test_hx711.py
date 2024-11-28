ca2c4d61babc38ff2e4a8ffeafcd3feb4c385ce6
zbumar17 <116588583+zbumar17@users.noreply.github.com>
2024-11-28 15:23:50 +0200

    test_hx711.py

---
+++ test_hx711.py
@@ -0,0 +1,39 @@
+import time
+import RPi.GPIO as GPIO
+from hx711 import HX711
+
+# Suppress GPIO warnings
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+# Define pins
+DT = 5  # Data pin
+SCK = 6  # Clock pin
+
+# Initialize HX711
+hx = HX711(DOUT=DT, PD_SCK=SCK)
+
+try:
+    # Reset HX711
+    hx.reset()
+
+    # Tare to set zero point
+    print("Taring... Remove all weights.")
+    hx.tare()
+    print("Tare complete. Place weight.")
+
+    while True:
+        # Read weight
+        raw_data = hx.get_weight_mean(5)  # Get an average of 5 readings
+        print(f"Weight: {raw_data} grams")
+
+        # Power down the HX711 between readings
+        hx.power_down()
+        hx.power_up()
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Exiting...")
+    GPIO.cleanup()
+
+
